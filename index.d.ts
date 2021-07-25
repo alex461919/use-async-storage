1518ccfc0f1bb8f271af1ec6577cb2f33f08be62
Alex Didenko <alex.461919@gmail.com>
2021-07-25 23:20:11 +0700

    name

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module '@alex461919/useAsyncStorage' {
+declare module '@alex461919/use-async-storage' {
     export function useAsyncStorage<S>(key: string, defaultValue: S): [S, (arg: S) => void, () => void];
     export function useAsyncStorage<S = undefined>(key: string): [S | undefined, (arg: S | undefined) => void, () => void];
 }
